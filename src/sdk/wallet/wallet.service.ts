0d9ca18a7760c8b54dfbba332a3e1821a25852d9
Stanisław Głogowski <stanislaw.glogowski@gmail.com>
2020-08-17 08:59:45 +0200

    fix: methods order

--- src/sdk/wallet/wallet.service.ts
+++ src/sdk/wallet/wallet.service.ts
@@ -4,19 +4,19 @@ import { Service } from '../common';
 export class WalletService extends Service {
   private signer: utils.SigningKey;
 
-  get address(): string {
-    return this.wallet ? this.wallet.address : null;
-  }
   constructor(private wallet: Wallet) {
     super();
 
     this.signer = new utils.SigningKey(wallet.privateKey);
   }
-
   protected onInit(): void {
     const { accountService } = this.services;
 
     accountService.createAccountFromWallet(this.wallet);
+  }
+
+  get address(): string {
+    return this.wallet ? this.wallet.address : null;
   }
 
   personalSignMessage(message: BytesLike): Promise<string> {
