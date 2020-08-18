4de68c72006631ad5902cc6a9dc81ac0b210bb09
Stanisław Głogowski <stanislaw.glogowski@gmail.com>
2020-08-18 12:56:12 +0200

    chore: update examples

--- examples/src/02-contract-account.ts
+++ examples/src/02-contract-account.ts
@@ -1,23 +1,22 @@
 import { Wallet } from 'ethers';
-import { take } from 'rxjs/operators';
 import { Sdk } from '../../src';
 import { logger } from './common';
 
 async function main(): Promise<void> {
-  // random wallet
   const wallet = Wallet.createRandom();
 
   logger.log('wallet', wallet.address);
 
   const sdk = new Sdk(wallet);
 
-  const notification = sdk.subscribeNotifications().pipe(take(1)).toPromise();
+  logger.log('key account', sdk.state.account);
 
-  const ensNode = await sdk.reserveENSName(`random${Date.now().toString(16)}.pillar.dev`);
+  logger.log('contract account', await sdk.computeContractAccount(false));
 
-  logger.log('ensNode', ensNode);
+  await sdk.syncAccount();
 
-  logger.log('notification', await notification);
+  logger.log('synced contract account', sdk.state.account);
+  logger.log('synced contract account member', sdk.state.accountMember);
 }
 
 main()
--- examples/src/03-ens.ts
+++ examples/src/03-ens.ts
@@ -1,18 +1,22 @@
 import { Wallet } from 'ethers';
+import { take } from 'rxjs/operators';
 import { Sdk } from '../../src';
 import { logger } from './common';
 
 async function main(): Promise<void> {
-  // random wallet
   const wallet = Wallet.createRandom();
 
   logger.log('wallet', wallet.address);
 
   const sdk = new Sdk(wallet);
 
-  const session = await sdk.createSession();
+  const notification = sdk.subscribeNotifications().pipe(take(1)).toPromise();
 
-  logger.log('session', session);
+  const ensNode = await sdk.reserveENSName(`random${Date.now().toString(16)}.pillar.dev`);
+
+  logger.log('ensNode', ensNode);
+
+  logger.log('last notification', await notification);
 }
 
 main()
