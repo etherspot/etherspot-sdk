11b4b6ee9a0442b38aa53dd1229c086e6b3c8bf0
Stanisław Głogowski <stanislaw.glogowski@gmail.com>
2020-08-21 14:19:27 +0200

    chore: update examples

--- examples/src/02-contract-account.ts
+++ examples/src/02-contract-account.ts
@@ -1,6 +1,6 @@
 import { Wallet } from 'ethers';
 import { Sdk } from '../../src';
-import { logger } from './common';
+import { logger, topUpAccount, getBalance, randomAddress } from './common';
 
 async function main(): Promise<void> {
   const wallet = Wallet.createRandom();
@@ -8,18 +8,29 @@ async function main(): Promise<void> {
   logger.log('wallet', wallet.address);
 
   const sdk = new Sdk(wallet);
+  const { state } = sdk;
 
-  logger.log('key account', sdk.state.account);
+  logger.log('key account', state.account);
 
   logger.log('contract account', await sdk.computeContractAccount(false));
 
   await sdk.syncAccount();
 
-  logger.log('synced contract account', sdk.state.account);
-  logger.log('synced contract account member', sdk.state.accountMember);
+  logger.log('synced contract account', state.account);
+  logger.log('synced contract account member', state.accountMember);
 
-  logger.log('get account', await sdk.getAccount(sdk.state.accountAddress));
-  logger.log('get account members', await sdk.getAccountMembers(sdk.state.accountAddress));
+  logger.log('get account', await sdk.getAccount(state.accountAddress));
+  logger.log('get account members', await sdk.getAccountMembers(state.accountAddress));
+
+  await topUpAccount(state.accountAddress, '0.5');
+
+  logger.log('contract account balance', await getBalance(state.accountAddress));
+
+  logger.log('batch #1', await sdk.batchAddAccountOwner(randomAddress()));
+  logger.log('batch #2', await sdk.batchAddAccountOwner(randomAddress()));
+  logger.log('estimated batch', await sdk.estimateBatch());
+
+  logger.log('relayed transaction', await sdk.submitBatch());
 }
 
 main()
--- examples/src/03-ens.ts
+++ examples/src/03-ens.ts
@@ -1,7 +1,7 @@
 import { Wallet } from 'ethers';
 import { take } from 'rxjs/operators';
 import { Sdk } from '../../src';
-import { logger } from './common';
+import { logger, topUpAccount } from './common';
 
 async function main(): Promise<void> {
   const wallet = Wallet.createRandom();
@@ -10,13 +10,24 @@ async function main(): Promise<void> {
 
   const sdk = new Sdk(wallet);
 
+  const { state } = sdk;
+
   const notification = sdk.subscribeNotifications().pipe(take(1)).toPromise();
+
+  logger.log('contract account', await sdk.computeContractAccount(false));
+
+  await topUpAccount(state.accountAddress, '0.5');
 
   const ensNode = await sdk.reserveENSName(`random${Date.now().toString(16)}.pillar.dev`);
 
   logger.log('ensNode', ensNode);
 
   logger.log('last notification', await notification);
+
+  logger.log('batch', await sdk.batchClaimENSNode(ensNode));
+  logger.log('estimated batch', await sdk.estimateBatch());
+
+  logger.log('relayed transaction', await sdk.submitBatch());
 }
 
 main()
