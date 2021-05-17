1e945b2a59ff99867b7e45e0eebdee23711874da
Rafael Cidade <rafael@pillarproject.io>
2021-05-17 15:39:40 +0100

    [BAC-898] Account dashboard (#41)
    * Add account dashboard classes and method
    
    * update class and args
    
    * add dto and method to sdk
    
    * Graphql schema

--- src/sdk/account/account.service.ts
+++ src/sdk/account/account.service.ts
@@ -2,7 +2,7 @@ import { gql } from '@apollo/client/core';
 import { Observable, combineLatest } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { HeaderNames, Service, SynchronizedSubject, keccak256 } from '../common';
-import { Account, AccountBalances, AccountMember, AccountMembers, Accounts } from './classes';
+import { Account, AccountBalances, AccountDashboard, AccountMember, AccountMembers, Accounts } from './classes';
 import { AccountMemberStates, AccountMemberTypes, AccountTypes } from './constants';
 
 export class AccountService extends Service {
@@ -235,6 +235,41 @@ export class AccountService extends Service {
         },
         models: {
           result: AccountBalances,
+        },
+      },
+    );
+
+    return result;
+  }
+
+  async getAccountDashboard(account: string, currency: string, days: number): Promise<AccountDashboard> {
+    const { apiService } = this.services;
+
+    const { result } = await apiService.query<{
+      result: AccountDashboard;
+    }>(
+      gql`
+        query($chainId: Int, $account: String!, $currency: String!, $days: Float!) {
+          result: accountDashboard(chainId: $chainId, account: $account, currency: $currency, days: $days) {
+            history {
+              balance
+              timestamp
+            }
+            wallet {
+              balance
+              netChange
+            }
+          }
+        }
+      `,
+      {
+        variables: {
+          account,
+          currency,
+          days,
+        },
+        models: {
+          result: AccountDashboard,
         },
       },
     );
