bd6534e58b0143b6817dd1bf5912802c0d7ae91b
Ardian <34317628+arddluma@users.noreply.github.com>
2021-06-07 16:11:16 +0200

    Implement pipeline (#49)
    * Implement pipeline
    
    * Add scripts based on review
    
    * 1.3.9

---
+++ .circleci/announcePublish.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -euo pipefail
+
+applicationName=$1
+package=$2
+
+payload=$(
+cat <<EOM
+{
+    "attachments": [
+        {
+            "fallback": "A new version of $applicationName is available for use. :circleci-pass:",
+            "color": "#33CC66",
+            "pretext": "A new version of $applicationName is available for use. :circleci-pass:",
+            "title": "$CIRCLE_PROJECT_REPONAME",
+            "title_link": "https://circleci.com/workflow-run/$CIRCLE_WORKFLOW_WORKSPACE_ID",
+            "text": "artifact published: $package",
+            "ts": $(date '+%s')
+        }
+    ]
+}
+EOM
+)
+
+curl -X POST --data-urlencode payload="$payload" "$SLACK_WEBHOOK_URL"
