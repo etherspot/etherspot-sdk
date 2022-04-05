b7dec433dd71115da5d40f093dbfaaa5483dc0dd
riccardo <ravaro.riccardo@gmail.com>
2022-04-05 09:01:13 +0100

    BAC-1196 transactions test update

--- examples/src/13-transactions.ts
+++ examples/src/13-transactions.ts
@@ -1,16 +1,35 @@
 import { Wallet } from 'ethers';
 import { EnvNames, NetworkNames, Sdk } from '../../src';
-import { logger } from './common';
+import { logger, deployToken, getPrivateKeyWallet, randomAddress, balanceOf, topUpAccount } from './common';
 
 async function main(): Promise<void> {
-  const wallet = Wallet.createRandom();
-  const sdk = new Sdk(wallet, {
-    env: EnvNames.MainNets,
-    networkName: NetworkNames.Mainnet,
-  });
+  const privateKey = '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80';
+  const wallet = getPrivateKeyWallet(privateKey, NetworkNames.LocalA);
+  const sdk = new Sdk(wallet);
+  await sdk.computeContractAccount({ sync: false });
+  await sdk.topUp('10');
 
-  const account = '';
-  const transactions = await sdk.getTransactions({ account });
+  const { state } = sdk;
+
+  logger.log(
+    'batch #1',
+    await sdk.batchAddAccountOwner({
+      owner: randomAddress(),
+    }),
+  );
+  logger.log(
+    'batch #2',
+    await sdk.batchAddAccountOwner({
+      owner: randomAddress(),
+    }),
+  );
+  logger.log('estimated batch', await sdk.estimateGatewayBatch());
+
+  logger.log('submitted batch', await sdk.submitGatewayBatch());
+
+
+  // // const account = '0xc7cE2cB91402B1249f5271c89b7e4fad3548Afe7';
+  const transactions = await sdk.getTransactions({ account:state.accountAddress });
 
   logger.log('transactions', transactions);
 }
