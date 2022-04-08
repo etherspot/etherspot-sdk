b74708043e13d49f0d3570ea5604b7cef74643d9
riccardo <ravaro.riccardo@gmail.com>
2022-04-08 19:45:05 +0100

    clean up

--- examples/src/common/specs/common.specs.ts
+++ examples/src/common/specs/common.specs.ts
@@ -4,99 +4,6 @@ import { Chain, AssetType, AssetData, ETH } from './types';
 
 export type Value = BigNumber | string | number;
 
-export type TxSettlementItem = {
-  symbol: string;
-  value: string | number;
-  hash: string;
-};
-
-export type TxPoolTogetherExtra = {
-  symbol: string;
-  decimals: number;
-  amount: string;
-};
-
-type TxWithdrawalExtra = {
-  paymentHash: string;
-};
-
-type EnsTransactionExtra = {
-  ensName: string;
-};
-
-export type SyntheticTransaction = {
-  transactionId: string;
-  fromAmount: number;
-  toAmount: number;
-  toAssetCode: string;
-  toAddress: string;
-  receiverEnsName?: string;
-};
-
-export type SyntheticTransactionExtra = {
-  syntheticTransaction: Partial<SyntheticTransaction>;
-};
-
-export type AaveExtra = {
-  symbol: string;
-  decimals: number;
-  amount: string;
-};
-
-export type TxSablierExtra = {
-  assetAddress: string;
-  amount: string;
-  contactAddress: string;
-  streamId: string;
-};
-
-// export type RariDepositExtra = {
-//   symbol: string;
-//   decimals: number;
-//   amount: number;
-//   rariPool: RariPool;
-//   rftMinted: string;
-// };
-
-// export type RariWithdrawExtra = {
-//   symbol: string;
-//   decimals: number;
-//   amount: number;
-//   rariPool: RariPool;
-//   rftBurned: string;
-// };
-
-// export type RariTransferExtra = {
-//   amount: string;
-//   rariPool: RariPool;
-//   contactAddress: string;
-// };
-
-export type RariClaimExtra = {
-  amount: string;
-  rgtBurned: string;
-};
-
-// export type RariExtra = RariDepositExtra | RariWithdrawExtra | RariTransferExtra | RariClaimExtra;
-
-// export type LiquidityPoolsExtra = {
-//   amount: string;
-//   pool: LiquidityPool;
-//   tokenAmounts?: string[];
-// };
-
-export type EtherspotTransactionExtra = {
-  batchHash: string;
-};
-
-export type AllowanceTransactionExtra = {
-  allowance: {
-    provider: string;
-    fromAssetCode: string;
-    toAssetCode: string;
-  };
-};
-
 
 export type GasToken = {
   address: string;
@@ -104,10 +11,6 @@ export type GasToken = {
   symbol: string;
 };
 
-export type FeeWithGasToken = {
-  feeInWei: BigNumber;
-  gasToken: GasToken;
-};
 
 export type TransactionPayload = {
   gasLimit?: number;
@@ -137,43 +40,7 @@ export type TransactionPayload = {
   chain?: Chain;
 };
 
-export type CollectibleTransactionPayload = {
-  to: string;
-  contractAddress: string;
-  tokenId: string;
-  useLegacyTransferMethod: boolean;
-  gasLimit?: number;
-  gasPrice?: number;
-  signOnly?: boolean;
-};
 
-// export type TransactionEthers = {
-//   from: string;
-//   hash?: string;
-//   batchHash?: string;
-//   to: string;
-//   value: string | Object;
-//   gasPrice?: Object | number;
-//   gasLimit?: Object | number;
-//   gasUsed?: Object | number;
-//   assetAddress: string;
-//   assetSymbol: string;
-//   note?: string;
-//   status?: string;
-//   createdAt?: number;
-//   isPPNTransaction?: boolean;
-//   tag?: string;
-//   extra?: TransactionExtra;
-//   stateInPPN?: string;
-//   feeWithGasToken?: FeeWithGasToken;
-//   type?: string;
-// };
-
-export type TransactionFeeInfo = {
-  fee: BigNumber;
-  gasToken?: GasToken;
-  gasPrice?: BigNumber;
-};
 
 export type EthereumTransaction = {
   to: string;
@@ -181,22 +48,4 @@ export type EthereumTransaction = {
   data?: string;
 };
 
-export type TransactionToEstimate = {
-  to: string;
-  value: Value;
-  assetData?: AssetData;
-  data?: string;
-};
 
-export type TransactionStatus = {
-  isSuccess: boolean;
-  error: string;
-  noRetry?: boolean;
-  hash?: string;
-  batchHash?: string;
-};
-
-export type TransactionResult = {
-  hash?: string;
-  batchHash?: string;
-};
--- examples/src/common/specs/types/Chains.types.ts
+++ examples/src/common/specs/types/Chains.types.ts
@@ -1,69 +1,7 @@
-// @flow
-/*
-    Pillar Wallet: the personal data locker
-    Copyright (C) 2021 Stiftung Pillar Project
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License along
-    with this program; if not, write to the Free Software Foundation, Inc.,
-    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-*/
-
-// Utils
-// import { isProdEnv } from 'utils/environment';
-
-// // Constants
 import { ETH, MATIC, BNB, AVAX, XDAI, ADDRESS_ZERO } from './constants/assetsConstants';
-import { CHAIN, CHAIN_ID } from './constants/chainConstants';
+import { CHAIN } from './constants/chainConstants';
 
-// // Utils
-// import { isEtherspotAccount } from 'utils/accounts';
-// import { mapRecordValues } from 'utils/object';
-
-// // Types
-// import type { Account } from 'models/Account';
-// import type { Chain, ChainRecord } from 'models/Chain';
-
-// export const chainFromChainId: { [number]: Chain } = {
-//   [CHAIN_ID.ETHEREUM_MAINNET]: CHAIN.ETHEREUM,
-//   [CHAIN_ID.ETHEREUM_KOVAN]: CHAIN.ETHEREUM,
-//   [CHAIN_ID.BINANCE]: CHAIN.BINANCE,
-//   [CHAIN_ID.XDAI]: CHAIN.XDAI,
-//   [CHAIN_ID.POLYGON]: CHAIN.POLYGON,
-//   [CHAIN_ID.AVALANCHE]: CHAIN.AVALANCHE,
-//   [CHAIN_ID.FUJI]: CHAIN.AVALANCHE,
-// };
-
-/**
-//  * Maps chain value to chain id, supporting testnet(s) for test env.
-//  */
-// export function mapChainToChainId(chain: Chain): number {
-//   if (chain === CHAIN.ETHEREUM) return isProdEnv() ? CHAIN_ID.ETHEREUM_MAINNET : CHAIN_ID.ETHEREUM_KOVAN;
-//   if (chain === CHAIN.POLYGON) return CHAIN_ID.POLYGON;
-//   if (chain === CHAIN.BINANCE) return CHAIN_ID.BINANCE;
-//   if (chain === CHAIN.XDAI) return CHAIN_ID.XDAI;
-//   if (chain === CHAIN.AVALANCHE) return isProdEnv() ? CHAIN_ID.AVALANCHE : CHAIN_ID.FUJI;
-
-//   // Default to Ethereum, should not happen as above check is exhaustive.
-//   return isProdEnv() ? CHAIN_ID.ETHEREUM_MAINNET : CHAIN_ID.ETHEREUM_KOVAN;
-// }
-
-// export function getSupportedChains(account: ?Account): Chain[] {
-//   if (!isEtherspotAccount(account)) {
-//     return [CHAIN.ETHEREUM];
-//   }
-
-//   return [CHAIN.POLYGON, CHAIN.BINANCE, CHAIN.XDAI, CHAIN.ETHEREUM, CHAIN.AVALANCHE];
-// }
 
 /* eslint-disable i18next/no-literal-string */
 export const nativeAssetPerChain = {
