83224bb99e942f1db55f0a8a365dd34b1ac843da
riccardo <ravaro.riccardo@gmail.com>
2022-04-11 12:41:12 +0100

    text update

--- examples/src/16-swap.ts
+++ examples/src/16-swap.ts
@@ -12,7 +12,7 @@ async function main(): Promise<void> {
 
   const sdkMainnet = new Sdk(wallet2, {
     env: EnvNames.MainNets,
-    networkName: NetworkNames.Mainnet,
+    networkName: NetworkNames.Matic,
   });
 
   const sdk = new Sdk(wallet, {
@@ -31,28 +31,30 @@ async function main(): Promise<void> {
   // NOTE: use ethers.constants.AddressZero for ETH
   // const fromTokenAddress = '0xD71eCFF9342A5Ced620049e616c5035F1dB98620'; // sEUR
   // const toTokenAddress = '0x5e74C9036fb86BD7eCdcb084a0673EFc32eA31cb'; // sETH
-  const fromTokenAddress = '0xe3818504c1b32bf1557b16c238b2e01fd3149c17'; // PLR
-  const toTokenAddress = constants.AddressZero; // ETH
+  const pillarToken = '0xe3818504c1b32bf1557b16c238b2e01fd3149c17'; // PLR
+  const wrappedMatic = '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270' // Polygon: WMATIC Token https://polygonscan.com/address/0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270
+  const PillarPostoken = '0xa6b37fC85d870711C56FbcB8afe2f8dB049AE774'; // Pillar: PLR Token 
+  const eth = constants.AddressZero; // ETH
   const fromAmount = '5000000000000000000000';
 
   const offers = await sdkMainnet.getExchangeOffers({
-    fromTokenAddress,
-    toTokenAddress,
+    fromTokenAddress:wrappedMatic,
+    toTokenAddress:PillarPostoken,
     fromAmount: BigNumber.from(fromAmount),
   });
 
-  const offers2 = await sdk.getExchangeOffers({
-    fromTokenAddress,
-    toTokenAddress,
-    fromAmount: BigNumber.from(fromAmount),
-  });
-  console.log('offers2 -----------------');
-  console.log(offers2);
+  // const offers2 = await sdk.getExchangeOffers({
+  //   fromTokenAddress,
+  //   toTokenAddress,
+  //   fromAmount: BigNumber.from(fromAmount),
+  // });
+  console.log('offers -----------------');
+  console.log(offers);
 
   // if(false) {
-  logger.log('exchange offers', offers);
+  logger.log('exchange offers', offers[0]);
 
-  const offerTransactions = offers[1].transactions;
+  const offerTransactions = offers[0].transactions;
   // logger.log('exchange offers 0', offers[0].transactions[1]);
   // logger.log('exchange offers 1', offers[1].transactions[1]);
   // logger.log('exchange offers 2', offers[2].transactions[1]);
