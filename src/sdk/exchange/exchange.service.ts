1fd682a326ecee4280a6f73afb21ce565c63ae2d
vignesha22 <vignesh@pillarproject.io>
2022-12-27 10:51:50 +0530

    return lifi bridge used in get quotes

--- src/sdk/exchange/exchange.service.ts
+++ src/sdk/exchange/exchange.service.ts
@@ -151,6 +151,7 @@ export class ExchangeService extends Service {
                   estimatedGas
                 }
               }
+              LiFiBridgeUsed
             }
           }
         }
